956b764b13c5d1c3113747ed4b64240f4dbbc2db
NicNewgate <stecker.nic@outlook.com>
2025-01-23 14:51:52 +0100

    added project - exercise: fibonacci-numbers

---
+++ fibonacciNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿/**
+* Microsoft Übung! 
+* Übungsbeschreibung:
+* gegeben:
+
+var fibonacciNumbers = new List<int> {1, 1};
+
+var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+fibonacciNumbers.Add(previous + previous2);
+
+foreach(var item in fibonacciNumbers)
+{
+    Console.WriteLine(item);
+}
+
+* Versuchen Sie, einige dieser Konzepte aus dieser Lektion 
+* und früheren Lektionen in einen Zusammenhang zu bringen. 
+* Erweitern Sie das, was Sie bisher bezüglich Fibonacci-Zahlen erstellt haben. 
+* Versuchen Sie, den Code zum Generieren der ersten 20 Zahlen 
+* der Sequenz zu schreiben. (Hinweis: Die 20. Fibonacci-Zahl lautet 6765.)
+**/
+
+//Backend
+var fibonacciNumbers = new List<int> { 1, 1 };
+int previous, preprevious;
+
+//index 0 und 1 sind schon in verwendung von daher starten wir mit 2
+for (int i = 2; i < 20; ++i)
+{
+    previous = fibonacciNumbers[i - 1];
+    preprevious = fibonacciNumbers[i - 2];
+
+    fibonacciNumbers.Add(previous + preprevious);
+}
+
+//Ausgabe
+foreach (var number in fibonacciNumbers)
+{
+    Console.WriteLine(number);
+}
