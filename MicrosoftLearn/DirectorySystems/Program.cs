25c1ef22d75efc9a2380037846522100081a44f1
NicNewgate <stecker.nic@outlook.com>
2025-01-28 17:04:37 +0100

    added directorysystems project and some practice funcs to work with an example directory

---
+++ MicrosoftLearn/DirectorySystems/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+
+// Hole den Pfad zum aktuellen Arbeitsverzeichnis
+string currentDirectory = Directory.GetCurrentDirectory();
+
+// Gib den Pfad aus
+Console.WriteLine("Das aktuelle Arbeitsverzeichnis ist: " + currentDirectory + "\n");
+
+// Gibt die erste Indexziffer an der das wort/der zu suchend string gefunden wurde wieder
+int index = currentDirectory.IndexOf("bin");
+
+if (index >= 0)
+{
+    // Schneide den Text ab dem Index des Wortes ab
+    string trimmedText = currentDirectory.Substring(0, index);
+    Console.WriteLine("Getrimmter Text: " + trimmedText + "\n");
+
+    // Enumeriere alle unterverzeichnisse innerhalb von "stores"
+    IEnumerable<string> listOfDirectories = Directory.EnumerateFiles(trimmedText + "stores", "*.txt", SearchOption.AllDirectories);
+
+    // Ausgabe der Verzeichnisse
+    foreach (string directory in listOfDirectories)
+    {
+        Console.WriteLine(directory);
+    }
+}
+else
+{
+    Console.WriteLine("Das Wort wurde nicht gefunden.");
+}
