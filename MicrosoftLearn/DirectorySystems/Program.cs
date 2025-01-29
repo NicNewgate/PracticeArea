4e0b5cc19e6434975a1cc244f3f537fa8dc7d423
NicNewgate <stecker.nic@outlook.com>
2025-01-29 17:21:05 +0100

    DirectorySystems: extended the project to read, write and update files, using Directory/Path -class and Json Nuget-Package

--- MicrosoftLearn/DirectorySystems/Program.cs
+++ MicrosoftLearn/DirectorySystems/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using static System.Net.Mime.MediaTypeNames;
+﻿// Hole den Pfad zum aktuellen Arbeitsverzeichnis
+using Newtonsoft.Json;
+using System.Text.Json;
 
-// Hole den Pfad zum aktuellen Arbeitsverzeichnis
 string currentDirectory = Directory.GetCurrentDirectory();
 
 // Gib den Pfad aus
@@ -30,4 +28,143 @@ if (index >= 0)
 else
 {
     Console.WriteLine("Das Wort wurde nicht gefunden.");
+}
+
+Console.WriteLine($"\n***************************************************\n");
+
+//*********************************- FindFiles -*****************************************************
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (string file in foundFiles)
+    {
+        // The file name will contain the full path, so only check the end of it
+        var extension = Path.GetExtension(file);
+        if (file.EndsWith(".json"))
+        {
+            salesFiles.Add(file);
+        }
+    } 
+    return salesFiles;
+}
+
+Console.WriteLine($"\n***************************************************\n");
+
+// further examples
+string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+Console.WriteLine(docPath);
+Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+Console.WriteLine(Path.Combine("stores", "201"));
+Console.WriteLine(Path.GetExtension("sales.json")); // outputs: .json
+
+Console.WriteLine($"\n***************************************************\n");
+
+// use path-class for directoryseperators - keep in mind that not all systems use the same separators
+string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+FileInfo info = new FileInfo(fileName);
+Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}"); // And many more
+
+Console.WriteLine($"\n***************************************************\n");
+
+// windows doesnt rly care about font or backslashes... but other environments do
+FileInfo a =  new FileInfo("C:/Users/steck/source/repos/PracticeArea/MicrosoftLearn/DirectorySystems/stores/204/inventory.txt");
+Console.WriteLine($"Full Name: {a.FullName}{Environment.NewLine}Directory: {a.Directory}{Environment.NewLine}Extension: {a.Extension}{Environment.NewLine}Create Date: {a.CreationTime}"); // And many more
+
+Console.WriteLine($"\n***************************************************\n");
+
+// Create, read, update, delete - Files -> Modern = CRUD, one example of creating
+var filePath = Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "stores", "201", "newDir")).ToString();
+
+
+// check if dir exits
+bool doesDirectoryExist = Directory.Exists(filePath);
+Console.WriteLine($"Existiert der Dateipfad? ->{doesDirectoryExist}");
+
+if( doesDirectoryExist )
+{
+    Console.WriteLine(filePath);
+}
+
+Console.WriteLine();
+
+// Create files in Directory and write
+var greetingFile = "greeting.txt";
+var greetingFilePath = Path.Combine(Directory.GetCurrentDirectory(), greetingFile);
+File.WriteAllText(greetingFilePath, "Hello World!");
+
+if (File.Exists(greetingFilePath))
+{
+    Console.WriteLine($"File \"{greetingFile}\" exits!");
+}
+
+Console.WriteLine($"\n***************************************************\n");
+
+// Further practice with FindFiles()
+
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+
+Directory.CreateDirectory(salesTotalDir);
+
+var salesFiles = FindFiles(storesDirectory);
+
+// Generate a txt files and write "" in it with String.Empty
+var salesTotalsFile = Path.Combine(salesTotalDir, "totals.txt");
+
+//***************************************************
+// Reading data from files
+var salesJ = File.ReadAllText($"{currentDirectory}{Path.DirectorySeparatorChar}stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales.json");
+Console.WriteLine(salesJ + "\n");
+
+// Reading with Newtonsoft.Json
+var salesData = JsonConvert.DeserializeObject<SalesTotal>(salesJ);
+Console.WriteLine(salesData.Total);
+
+Console.WriteLine($"\n***************************************************\n");
+
+//***************************************************
+// Writing data into files
+File.WriteAllText(salesTotalsFile, $"{salesData.Total}{Environment.NewLine}");
+// Update data in files
+File.AppendAllText(salesTotalsFile, $"{salesData.Total}{Environment.NewLine}");
+
+//***************************************************
+// New Function to calculate all data
+var salesTotal = CalculateSalesTotal(salesFiles);
+//File.WriteAllText(salesTotalsFile, String.Empty);
+File.AppendAllText(salesTotalsFile, $"{salesTotal}{Environment.NewLine}");
+
+
+// Reading and Writeing data
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+
+    //READ FILES LOOP
+    foreach (var file in salesFiles)
+    {
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+
+        // Parse the contents as JSON
+        SalesData? data = System.Text.Json.JsonSerializer.Deserialize<SalesData?>(salesJson);
+
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+
+// *** PROPERTIES ***
+
+record SalesData (double Total);
+
+class SalesTotal
+{
+    public double Total { get; set; }
 }
