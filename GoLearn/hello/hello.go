5ff7ea029c3e75a296b96200446231b3cfc19cbb
NicNewgate <stecker.nic@outlook.com>
2025-02-01 16:43:50 +0100

    GoLearn: Added error Handling and print out a random message

--- GoLearn/hello/hello.go
+++ GoLearn/hello/hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"rsc.io/quote"
 
@@ -12,7 +13,27 @@ func main() {
 	fmt.Println("Hello, World!\n")
 	fmt.Println(quote.Go()+"\n")
 	
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	// Error Handling
+
+	// Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+	log.SetPrefix("log-greetings: ");
+	log.SetFlags(0)
+
+	// Request a greeting message
+	message, err := greetings.Hello("Gladys")
+	// If an error was returned, print it to the console and
+    // exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+    // to the console.
 	fmt.Println(message)
+
+	// Old Code: Get a greeting message and print it.
+	//message := greetings.Hello("Gladys")
+	//fmt.Println(message)
 }
