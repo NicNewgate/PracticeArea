568ef4fa0f0308ed25c13e3749b1fcda90fd996a
NicNewgate <stecker.nic@outlook.com>
2025-02-01 15:56:52 +0100

    Added new greetings module and used it in the hello module to print out hello world message.

--- GoLearn/hello/hello.go
+++ GoLearn/hello/hello.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-import "rsc.io/quote"
+	"rsc.io/quote"
+
+	"example.com/greetings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println(quote.Go())
+	fmt.Println("Hello, World!\n")
+	fmt.Println(quote.Go()+"\n")
+	
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
