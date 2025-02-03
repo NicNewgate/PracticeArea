d339b717438223ccabf5582054e7317f2495bb92
NicNewgate <stecker.nic@outlook.com>
2025-02-03 14:29:56 +0100

    GoLearn_ refactoring

--- GoLearn/greetings/greetings.go
+++ GoLearn/greetings/greetings.go
@@ -1,16 +1,16 @@
 package greetings
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
 	"math/rand"
 )
 
 // Hello returns a greeting for the named person
-func Hello(name string) (string,error) {
+func Hello(name string) (string, error) {
 	// If no name was given , return an error with a message
 	if name == "" {
-		return "", errors.New("empty name")	
+		return "", errors.New("empty name")
 	}
 
 	// Return a greeting that embeds the name in a message
@@ -25,10 +25,10 @@ func randomFormat() string {
 	formats := []string{
 		"Hi, %v. Welcome!",
 		"Great to see you, %v!",
-        "Hail, %v! Well met!",
+		"Hail, %v! Well met!",
 	}
 
 	// Return a randomly selected message format by specifying
-    // a random index for the slice of formats.
+	// a random index for the slice of formats.
 	return formats[rand.Intn(len(formats))]
 }
--- GoLearn/hello/hello.go
+++ GoLearn/hello/hello.go
@@ -10,27 +10,29 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!\n")
-	fmt.Println(quote.Go()+"\n")
-	
+	fmt.Println("Hello, World!")
+	fmt.Println()
+	fmt.Println(quote.Go())
+	fmt.Println()
+
 	// Error Handling
 
 	// Set properties of the predefined Logger, including
-    // the log entry prefix and a flag to disable printing
-    // the time, source file, and line number.
-	log.SetPrefix("log-greetings: ");
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("log-greetings: ")
 	log.SetFlags(0)
 
 	// Request a greeting message
 	message, err := greetings.Hello("Gladys")
 	// If an error was returned, print it to the console and
-    // exit the program.
+	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// If no error was returned, print the returned message
-    // to the console.
+	// to the console.
 	fmt.Println(message)
 
 	// Old Code: Get a greeting message and print it.
