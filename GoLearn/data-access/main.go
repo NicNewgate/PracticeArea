5a7c974c9e29d1c11c339d20b6a250c06b33fd5c
NicNewgate <stecker.nic@outlook.com>
2025-02-05 17:17:06 +0100

    GoLearn: added crud funcs to data-access project

--- GoLearn/data-access/main.go
+++ GoLearn/data-access/main.go
@@ -45,6 +45,31 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Albums found: %v\n", albums)
+
+	// Get an album via id - we use id 3
+	album, err := albumByID(3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album with ID 3: %v\n", album)
+
+	// Add an album to our data-table
+	albID, err := addAlbum(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID of added album: %v\n", albID)
+
+	// Delete an album from our data-table
+	errMes := deleteAlbum(5)
+	if errMes != nil {
+		log.Fatal(errMes)
+	}
+	fmt.Printf("Deleted last generated Dataset with ID: %v", 5)
 }
 
 // albumsByArtist queries for albums that have the specified artist name.
@@ -69,4 +94,43 @@ func albumsByArtist(name string) ([]Album, error) {
 		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
 	}
 	return albums, nil
+}
+
+// albumByID queries for the album with the specified ID.
+func albumByID(id int64) (Album, error) {
+	// An album to hold data from the returned row.
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsByID %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsByID %d: %v", id, err)
+	}
+	return alb, nil
+}
+
+// addAlbum adds the specified album to the database,
+// returning the album ID of the new entry
+func addAlbum(alb Album) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?,?,?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	return id, nil
+}
+
+// deletes the specified album from the database,
+// returning true when deleted
+func deleteAlbum(id int64) error {
+	_, err := db.Exec("DELETE FROM album WHERE id = ?", id)
+	if err != nil {
+		return fmt.Errorf("deleteAlbum: %v", err)
+	}
+	return nil
 }
